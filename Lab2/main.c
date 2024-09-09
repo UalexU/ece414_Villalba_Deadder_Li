93e2a5b118268b12146aede85b5c26086320fdc7
Alex <48929451+UalexU@users.noreply.github.com>
2024-09-09 13:16:24 -0400

    Lab_2 v 0.02

--- Lab2/main.c
+++ Lab2/main.c
@@ -2,28 +2,34 @@
 #include "sw_in.h"
 #include "led_out.h"
 #include <cstdint>
+#include <stdbool.h>
 
 main(){
     bool int1, int2;
     uint16_t out;
     sw_in_init();
     led_out_init();
-
+    
     while(1){
+        
         int1 = sw_in_read1();
         int2 = sw_in_read2();
 
         if ( int1 == true && int2 == true){
             // turn on 8 leds
-            led_out_write(); 
+            led_out_write(0xFF); 
         }
         else if (int1 == true && int2 == false){
+             // turn on left most led
+            led_out_write(0x10); 
+        }
+        else if (int1 == false && int2 == true){
              // turn on right most led
-            led_out_write(); 
+            led_out_write(0x01); 
         }
         else {
              // turn off all led
-            led_out_write(); 
+            led_out_write(0x00); 
         }
     }
 }
